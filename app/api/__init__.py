250279c57a4fe7144203dd09aa4f0c4cca6af43a
Mark <krotovod777@gmail.com>
2022-03-30 10:56:24 +0300

    feat(api): add routers

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 
 from app.api.authorization import auth_router
 from app.api.user import user_router
+from app.api.game import game_router
 
 router = APIRouter()
 
 router.include_router(auth_router)
 router.include_router(user_router)
+router.include_router(game_router)
 
