969cf8d94411a7266d6911a6fe988c9193f9e838
Mark <krotovod777@gmail.com>
2022-03-31 17:30:28 +0300

    feat(managers): add methods - add method to show gift recipient info - add methods to generate participants pairs - add method to get games list

--- app/depends/managers/game_manager.py
+++ app/depends/managers/game_manager.py
@@ -1,5 +1,5 @@
 from copy import deepcopy
-from typing import Generic, Type, Optional, Union
+from typing import Generic, Type, Union
 
 from datetime import datetime
 import random
@@ -7,7 +7,7 @@ import random
 from fastapi import Depends
 from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
 
-from sqlalchemy import insert, update, select, and_, join
+from sqlalchemy import insert, update, select, and_, join, func
 from sqlalchemy.engine import Row
 
 from app.core.db import database
@@ -100,6 +100,10 @@ class GameManager(Generic[G]):
             )
         )
         users = await database.fetch_all(query)
+
+        for user in users:
+            await self.change_user_game_status(user)
+
         if len(users) < 2:
             raise Exception("There are too low users in the game.")
 
@@ -110,8 +114,11 @@ class GameManager(Generic[G]):
         participants_pairs = []
         for user_id, user_value in users_dict.items():
             valid_gift_recipients_dict = self.get_valid_gift_recipients(user_id, users_dict)
-            gift_recipient_id = random.choice(list(valid_gift_recipients_dict.keys()))
-            users_dict[gift_recipient_id]["is_gift_recipient"] = True
+            if len(list(valid_gift_recipients_dict.keys())) != 0:
+                gift_recipient_id = random.choice(list(valid_gift_recipients_dict.keys()))
+                users_dict[gift_recipient_id]["is_gift_recipient"] = True
+            else:
+                gift_recipient_id = None
 
             participants_pair = {
                 "secret_santa_id": user_id,
@@ -143,13 +150,117 @@ class GameManager(Generic[G]):
 
         return await database.fetch_all(query)
 
+    async def get_games_list(self) -> list:
+        query_join = join(
+            self.games_db_model,
+            self.participants_db_model,
+            self.games_db_model.id == self.participants_db_model.game_id
+        )
+        query = select(
+            self.games_db_model,
+            func.count(self.participants_db_model.id).label("participants_pairs")
+        ).select_from(
+            query_join
+        ).group_by(self.participants_db_model.game_id)
+        games = await database.fetch_all(query)
+        return [
+            {
+                "game_year": game["game_year"],
+                "is_registration_open": game["is_registration_open"],
+                "is_registration_close": game["is_registration_close"],
+                "participants_pairs": game["participants_pairs"],
+            }
+            for game in games
+        ]
+
+    async def get_participants_pairs(self, selected_year: int) -> list:
+        query_join = join(
+            self.games_db_model,
+            self.participants_db_model,
+            self.games_db_model.id == self.participants_db_model.game_id
+        )
+        query = select(
+            self.participants_db_model
+        ).select_from(
+            query_join
+        ).where(
+            self.games_db_model.game_year == selected_year
+        )
+        participants_pairs = await database.fetch_all(query)
+
+        query = select(
+            self.users_db_model
+        ).where(
+            self.users_db_model.is_verified == True
+        )
+        users = await database.fetch_all(query)
+        users_dict = {
+            user["id"]: {
+                "first_name": user["first_name"],
+                "last_name": user["last_name"]
+            }
+            for user in users
+        }
+        return [
+            {
+                "secret_santa_full_name": " ".join((
+                    users_dict[participants_pair["secret_santa_id"]]["first_name"],
+                    users_dict[participants_pair["secret_santa_id"]]["last_name"]
+                )),
+                "gift_recipient_full_name":  " ".join((
+                    users_dict[participants_pair["gift_recipient_id"]]["first_name"],
+                    users_dict[participants_pair["gift_recipient_id"]]["last_name"]
+                )),
+                "preferences": participants_pair["preferences"]
+            }
+            for participants_pair in participants_pairs
+        ]
+
+    async def change_user_game_status(self, user: UD):
+        query = update(
+            self.users_db_model
+        ).values({
+            "is_playing": False
+        }).where(
+            self.users_db_model.id == user.id
+        )
+
+        await database.execute(query)
+
+    async def get_gift_recipient_info(self, user: UD) -> Union[Row, None]:
+        query_join = join(
+            self.participants_db_model,
+            self.games_db_model,
+            self.participants_db_model.game_id == self.games_db_model.id
+        ).join(
+            self.users_db_model,
+            self.participants_db_model.gift_recipient_id == self.users_db_model.id
+        )
+
+        query = select(
+            self.users_db_model.first_name,
+            self.users_db_model.last_name,
+            self.users_db_model.department,
+            self.users_db_model.city,
+            self.users_db_model.street,
+            self.users_db_model.building,
+            self.participants_db_model.preferences
+        ).where(
+            self.participants_db_model.secret_santa_id == user.id
+        ).select_from(
+            query_join
+        )
+
+        return await database.fetch_one(query)
+
     @staticmethod
     def get_valid_gift_recipients(user_id: str, users: dict) -> dict:
         return {
-            k: v for k, v in users.items() if
+            k: v
+            for k, v in users.items() if
             k != user_id and
-            k not in users[k]["was_santa_for"] and
-            users[k]["is_gift_recipient"] is False
+            k not in users[user_id]["was_santa_for"] and
+            v["is_gift_recipient"] is False
         }
 
     @staticmethod
