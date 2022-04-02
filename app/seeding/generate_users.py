944b655193429a019554c2651988ab3b9e8d0955
Mark <krotovod777@gmail.com>
2022-04-02 12:50:47 +0300

    feat(*): add functions to seeding users

--- app/seeding/generate_users.py
+++ app/seeding/generate_users.py
@@ -1,27 +1,31 @@
 from uuid import uuid4
+import random as rand
 
 from faker import Faker
 from fastapi_users.password import get_password_hash
 
 from app.core.db import database
-from app.core.factory import Application
 from app.models import User
 
 fake = Faker('uk_UA')
 
+departments_list = ["Develop", "PM", "HR", "Service"]
+login_list = []
+email_list = []
 
-async def generate_users():
+
+async def generate_users(range_: int):
     await database.connect()
-    for i in range(100):
+    for i in range(range_):
         values = {
             "id": uuid4(),
-            "login": fake.first_name(),
-            "email": fake.email(),
+            "login": create_fake_login(),
+            "email": create_fake_email(),
             "hashed_password": get_password_hash("TestingPassword"),
             "first_name": fake.first_name(),
             "last_name": fake.last_name(),
             "is_playing": True,
-            "department": "IT",
+            "department": rand.choice(departments_list),
             "city": fake.city(),
             "street": fake.address().split(",")[0],
             "building": fake.building_number(),
@@ -32,3 +36,19 @@ async def generate_users():
         }
         await database.execute(query=User.__table__.insert(), values=values)
     await database.disconnect()
+
+
+def create_fake_login():
+    while True:
+        login = fake.first_name()
+        if login not in login_list:
+            login_list.append(login)
+            return login
+
+
+def create_fake_email():
+    while True:
+        email = fake.email()
+        if email not in email_list:
+            email_list.append(email)
+            return email
